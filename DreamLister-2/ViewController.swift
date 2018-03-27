42e91e9b7c9bee9c8625528478d4bc6776bf59ee
Your Name <melis.alibi2000@gmail.com>
2018-03-27 21:11:22 +0600

    Initial Commit

---
+++ DreamLister-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DreamLister-2
+//
+//  Created by Alibi on 27.03.2018.
+//  Copyright © 2018 Alibi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
